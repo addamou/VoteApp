bf66184938a25ace963ada22bd739ac0a58b5c88
Addamou <adamou.halidou@codeloccol.org>
2020-10-10 14:26:17 +0100

    Updates

---
+++ precache-manifest.7417391e17878c88f1976240a0121cff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9b62f535c523f3d15ac5a5c1b16193",
+    "url": "/VoteApp/index.html"
+  },
+  {
+    "revision": "42aa721dbe02918baafa",
+    "url": "/VoteApp/static/css/main.7e9553db.chunk.css"
+  },
+  {
+    "revision": "f056b02324e7fd3c8fac",
+    "url": "/VoteApp/static/js/2.53ae7a47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VoteApp/static/js/2.53ae7a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42aa721dbe02918baafa",
+    "url": "/VoteApp/static/js/main.40d39e5a.chunk.js"
+  },
+  {
+    "revision": "d6b8ec4aecdc100c1281",
+    "url": "/VoteApp/static/js/runtime-main.69572183.js"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/VoteApp/static/media/clogo.f278be79.png"
+  },
+  {
+    "revision": "33efcd5c2e0a5a833f67eec69743c422",
+    "url": "/VoteApp/static/media/html.33efcd5c.jpg"
+  },
+  {
+    "revision": "318286e7d372573ebc382aba8509890d",
+    "url": "/VoteApp/static/media/htmllogo.318286e7.png"
+  },
+  {
+    "revision": "82bdee7bd022bd5b5607c1ce798f0569",
+    "url": "/VoteApp/static/media/joomla.82bdee7b.png"
+  },
+  {
+    "revision": "4929b3703a1a7082d99b53eb1bbfc31f",
+    "url": "/VoteApp/static/media/react.4929b370.png"
+  },
+  {
+    "revision": "88c6db2777c496ec71274409c8239b20",
+    "url": "/VoteApp/static/media/wordpresslogo.88c6db27.png"
+  }
+]);
